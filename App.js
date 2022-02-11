640f5618c214241c50db93431508612c4b6ac1e9
basitaliMD <79150912+basitaliMD@users.noreply.github.com>
2022-02-11 18:11:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import LoginScreen from "./screens/loginScreen";
+import LoadingScreen from "./screens/loadingScreen";
+import DashboardScreen from "./screens/DashBoardScreen";
+import firebase from "firebase";
+import { firebaseConfig } from "./config";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen: LoadingScreen,
+  LoginScreen: LoginScreen,
+  DashboardScreen: DashboardScreen
+})
+
+const AppNavigator = createAppContainer(AppSwitchNavigator);
+
+export default function App() {
+  return <AppNavigator />;
+}
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA0qcSMPHo_itClkuoPQvM5bv2kM77xABI",
+  authDomain: "personal-details-94348.firebaseapp.com",
+  databaseURL: "https://personal-details-94348-default-rtdb.firebaseio.com",
+  projectId: "personal-details-94348",
+  storageBucket: "personal-details-94348.appspot.com",
+  messagingSenderId: "890451513665",
+  appId: "1:890451513665:web:cbd5329cbac05589bfd1c0"
+};
